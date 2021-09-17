6ff00feafd25ad5d28a59ba537b424fe66dc0ad0
Tobias Haider <thai@kollabor.at>
2021-09-17 11:44:30 +0200

    fixes for webpack

--- resources/default/scripts/semorg.js
+++ resources/default/scripts/semorg.js
@@ -1,5 +1,5 @@
 import 'bootstrap/js/src/util'
 import 'bootstrap'
-import '../../../../../skins/Tweeki/resources/scripts/tweeki'
+import '../../../../../skins/Tweeki/resources/scripts/tweeki/tweeki'
 
 jQuery(document).ready(function ($) {})
